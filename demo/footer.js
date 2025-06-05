84b015336fc847af1b9791e9a6ceb34eb790559b
prabhats2 <prabhatnlh@gmail.com>
2025-06-05 14:19:50 +0530

    index

--- demo/footer.js
+++ demo/footer.js
@@ -4,13 +4,13 @@ document.write(`
         <div class="container-fluid bg-dark text-white-50 footer pt-5 mt-5 wow fadeIn" data-wow-delay="0.1s"  >
             <div class="container py-5">
                 <div class="row g-5">
-                    <div class="col-lg-3 col-md-6">
+                    <div class="col-lg-4 col-md-6">
                         <h3 class="text-white mb-4">Get In Touch</h3>
                         <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>B1, 101, Cherry County, Noida Extension, Greater Noida West-201306
                             Landmark: Near Ryan International School</p>
-                        <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>+91 9759033533</p>
-                        <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>+91 8367913533</p>
-                        <p class="mb-2"style="font-size: 15px; text-decoration: bold;"><i class="fa fa-envelope me-3 "></i>bonjourplayschool@gmail.com</p>
+                        <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>+ 91 8376913533</p>
+                        <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>+91 9211724195</p>
+                        <p class="mb-2"style="font-size: 15px; text-decoration: bold;"><i class="fa fa-envelope me-3 "></i>bonjourkidzplayschool@gmail.com </p>
                         <div class="d-flex pt-2">
                             <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-twitter"></i></a>
                             <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-facebook-f"></i></a>
@@ -18,7 +18,7 @@ document.write(`
                             <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-linkedin-in"></i></a>
                         </div>
                     </div>
-                    <div class="col-lg-3 col-md-6">
+                    <div class="col-lg-4 col-md-6">
                         <h3 class="text-white mb-4">Quick Links</h3>
                         <a class="btn btn-link text-white-50" href="">About Us</a>
                         <a class="btn btn-link text-white-50" href="">Contact Us</a>
@@ -26,30 +26,8 @@ document.write(`
                         <a class="btn btn-link text-white-50" href="">Privacy Policy</a>
                         <a class="btn btn-link text-white-50" href="">Terms & Condition</a>
                     </div>
-                    <div class="col-lg-3 col-md-6">
-                        <h3 class="text-white mb-4">Our Branches</h3>
-                        <div class="row g-2 pt-2">
-                            <div class="col-4">
-                                <img class="img-fluid rounded bg-light p-1" src="img/classes-1.jpg" alt="">
-                            </div>
-                            <div class="col-4">
-                                <img class="img-fluid rounded bg-light p-1" src="img/classes-2.jpg" alt="">
-                            </div>
-                            <div class="col-4">
-                                <img class="img-fluid rounded bg-light p-1" src="img/classes-3.jpg" alt="">
-                            </div>
-                            <div class="col-4">
-                                <img class="img-fluid rounded bg-light p-1" src="img/classes-4.jpg" alt="">
-                            </div>
-                            <div class="col-4">
-                                <img class="img-fluid rounded bg-light p-1" src="img/classes-5.jpg" alt="">
-                            </div>
-                            <div class="col-4">
-                                <img class="img-fluid rounded bg-light p-1" src="img/classes-6.jpg" alt="">
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-md-6">
+                  
+                    <div class="col-lg-4 col-md-6">
                         <h3 class="text-white mb-4">Location</h3>
                         <iframe 
                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3502.9634727177536!2d77.4460608!3d28.6008726!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390cee5fc3ffffff%3A0x8c869ff033944ed0!2sBonjour%20Kidz%20Play%20School%20%26%20Day%20Care!5e0!3m2!1sen!2sin!4v1746683440591!5m2!1sen!2sin" 
@@ -193,3 +171,26 @@ style.innerHTML = `
   }
 `;
 document.head.appendChild(style);
+/*   <div class="col-lg-3 col-md-6">
+                        <h3 class="text-white mb-4">Our Branches</h3>
+                        <div class="row g-2 pt-2">
+                            <div class="col-4">
+                                <img class="img-fluid rounded bg-light p-1" src="img/classes-1.jpg" alt="">
+                            </div>
+                            <div class="col-4">
+                                <img class="img-fluid rounded bg-light p-1" src="img/classes-2.jpg" alt="">
+                            </div>
+                            <div class="col-4">
+                                <img class="img-fluid rounded bg-light p-1" src="img/classes-3.jpg" alt="">
+                            </div>
+                            <div class="col-4">
+                                <img class="img-fluid rounded bg-light p-1" src="img/classes-4.jpg" alt="">
+                            </div>
+                            <div class="col-4">
+                                <img class="img-fluid rounded bg-light p-1" src="img/classes-5.jpg" alt="">
+                            </div>
+                            <div class="col-4">
+                                <img class="img-fluid rounded bg-light p-1" src="img/classes-6.jpg" alt="">
+                            </div>
+                        </div>
+                    </div> */
