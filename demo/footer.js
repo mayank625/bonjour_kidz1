ed967c6ec36c1749ae581fe0cca912d074317bb2
prabhats2 <prabhatnlh@gmail.com>
2025-06-06 17:09:47 +0530

    code push

--- demo/footer.js
+++ demo/footer.js
@@ -1,95 +1,74 @@
 
 document.write(`
-     <!-- Footer Start -->
-        <div class="container-fluid bg-dark text-white-50 footer pt-5 mt-5 wow fadeIn" data-wow-delay="0.1s"  >
-            <div class="container py-5">
-                <div class="row g-5">
-                    <div class="col-lg-4 col-md-6">
-                        <h3 class="text-white mb-4">Get In Touch</h3>
-                        <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>B1, 101, Cherry County, Noida Extension, Greater Noida West-201306
-                            Landmark: Near Ryan International School</p>
-                        <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>+ 91 8376913533</p>
-                        <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>+91 9211724195</p>
-                        <p class="mb-2"style="font-size: 15px; text-decoration: bold;"><i class="fa fa-envelope me-3 "></i>bonjourkidzplayschool@gmail.com </p>
-                        <div class="d-flex pt-2">
-                            <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-twitter"></i></a>
-                            <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-facebook-f"></i></a>
-                            <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-youtube"></i></a>
-                            <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-linkedin-in"></i></a>
-                        </div>
-                    </div>
-                    <div class="col-lg-4 col-md-6">
-                        <h3 class="text-white mb-4">Quick Links</h3>
-                        <a class="btn btn-link text-white-50" href="">About Us</a>
-                        <a class="btn btn-link text-white-50" href="">Contact Us</a>
-                        <a class="btn btn-link text-white-50" href="">Our Services</a>
-                        <a class="btn btn-link text-white-50" href="">Privacy Policy</a>
-                        <a class="btn btn-link text-white-50" href="">Terms & Condition</a>
-                    </div>
-                  
-                    <div class="col-lg-4 col-md-6">
-                        <h3 class="text-white mb-4">Location</h3>
-                        <iframe 
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3502.9634727177536!2d77.4460608!3d28.6008726!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390cee5fc3ffffff%3A0x8c869ff033944ed0!2sBonjour%20Kidz%20Play%20School%20%26%20Day%20Care!5e0!3m2!1sen!2sin!4v1746683440591!5m2!1sen!2sin" 
-                            width="100%" 
-                            height="200" 
-                            style="border-radius:5px;" 
-                            allowfullscreen="" 
-                            loading="lazy" 
-                            referrerpolicy="no-referrer-when-downgrade">
-                        </iframe>
-
-                    </div>
+     <!-- Footer Start --><!-- Footer Start -->
+<div class="container-fluid bg-dark text-white-50 footer pt-5 mt-5 wow fadeIn" data-wow-delay="0.1s" style="background-color: #1a1a1a;">
+    <div class="container py-5">
+        <div class="row g-5">
+            <div class="col-lg-4 col-md-6">
+                <h3 class="text-white mb-4">Get In Touch</h3>
+                <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>B1, 101, Cherry County, Noida Extension, Greater Noida West-201306 Landmark: Near Ryan International School</p>
+                <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>+ 91 8376913533</p>
+                <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>+91 9211724195</p>
+                <p class="mb-2" style="font-size: 15px; text-decoration: bold;"><i class="fa fa-envelope me-3"></i>bonjourkidzplayschool@gmail.com</p>
+                <div class="d-flex pt-2">
+                    <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-twitter"></i></a>
+                    <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-facebook-f"></i></a>
+                    <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-youtube"></i></a>
+                    <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-linkedin-in"></i></a>
                 </div>
             </div>
-            <div class="container">
-                <div class="copyright">
-                    <div class="row">
-                        <div class="col-sm-6 text-center text-md-start mb-3 mb-md-0">
-                         <p style="font-size: smaller;">© 2025 Bonjour Kidz Play School & Day Care. All rights reserved | Design by <span style="color: #008CBA;"> <a href=""> EGT</a></span>
-                            </p>  
-							
-						
-                        </div>
-                        <div class="col-md-6 text-center text-md-end">
-                            <div class="footer-menu">
-                                <a href="">Home</a>
-                                <a href="">Cookies</a>
-                                <a href="">Help</a>
-                                <a href="">FQAs</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+            <div class="col-lg-4 col-md-6">
+                <h3 class="text-white mb-4">Quick Links</h3>
+                <a class="btn btn-link text-white-50" href="about.html">About Us</a>
+                <a class="btn btn-link text-white-50" href="contact.html">Contact Us</a>
+                <a class="btn btn-link text-white-50" href="programs.html">Our Programs</a>
+                <a class="btn btn-link text-white-50" href="#">Privacy Policy</a>
+                <a class="btn btn-link text-white-50" href="#">Terms & Condition</a>
             </div>
-            
+            <div class="col-lg-4 col-md-6">
+                <h3 class="text-white mb-4">Location</h3>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3502.9634727177536!2d77.4460608!3d28.6008726!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390cee5fc3ffffff%3A0x8c869ff033944ed0!2sBonjour%20Kidz%20Play%20School%20%26%20Day%20Care!5e0!3m2!1sen!2sin!4v1746683440591!5m2!1sen!2sin" width="100%" height="200" style="border-radius:5px;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
         </div>
-        
-  <!-- futr -->
-  <div class="futr">
+    </div>
+    <div class="container">
+        <div class="copyright">
+            <div class="row">
+                <div class="col-sm-6 text-center text-md-start mb-3 mb-md-0">
+                    <p style="font-size: smaller;">© 2025 Bonjour Kidz Play School & Day Care. All rights reserved | Design by <span style="color: #008CBA;"><a href=""> EGT</a></span></p>
+                </div>
+                <div class="col-md-6 text-center text-md-end">
+                    <div class="footer-menu">
+                        <a href="#">Home</a>
+                        <a href="#">Cookies</a>
+                        <a href="#">Help</a>
+                        <a href="#">FQAs</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- futr -->
+<div class="futr">
     <!-- Social Media Icons -->
-    <div class="social-icons">   </div>
-
-
+    <div class="social-icons"></div>
     <!-- Action Links with icons and slash -->
     <div class="action-links">
-                      <!-- Join Us Button -->
-   <a href="#" class="btn btn-primary rounded-pill px-3" data-bs-toggle="modal" data-bs-target="#admission">
-      <img src="img/admission_icon.png" alt="Admission Icon" /> Admission Enquiry 
-   </a>   
-   
-      <div class="slash">|</div>
-
-      <a href="franchise.html"><img src="img/start-a-franchise.png" alt="Franchise Icon" /> Start a Franchise</a>
+        <!-- Join Us Button -->
+        <a href="#" class="btn btn-primary rounded-pill px-3" data-bs-toggle="modal" data-bs-target="#admission">
+            <img src="img/admission_icon.png" alt="Admission Icon" /> Admission Enquiry
+        </a>
+        <div class="slash">|</div>
+        <a href="franchise.html"><img src="img/start-a-franchise.png" alt="Franchise Icon" /> Start a Franchise</a>
     </div>
-
     <!-- Right End Section -->
-    <div class="right-end">  </div>
-      
-    
-  </div>
-    <!-- futr END -->
+    <div class="right-end"></div>
+</div>
+<!-- futr END -->
 
+
+         
 <!-- Footer End -->
 
 <!-- Modal -->
@@ -104,7 +83,7 @@ document.write(`
                 <div class="modal-body">
                    
 
-<div class="form-wrapper">
+<div class="form-wrapper "  >
   <form id="admission">
     <div class="mb-3">
       <label for="name" class="form-label">
@@ -169,7 +148,8 @@ style.innerHTML = `
     padding: 30px;
     border-radius: 15px;
   }
-`;
+    
+ `;
 document.head.appendChild(style);
 /*   <div class="col-lg-3 col-md-6">
                         <h3 class="text-white mb-4">Our Branches</h3>
